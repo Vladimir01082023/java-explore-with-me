ee0ba85acfdda60cf31cd0373c3e1a544465b1e4
Vladimir01082023 <vladimir-malyshev-2014@mail.ru>
2024-09-03 12:18:07 +0300

    fixing problem with lombok and jpa-entity

--- stats-service/stats-server/src/main/java/ru/practicum/model/Stats.java
+++ stats-service/stats-server/src/main/java/ru/practicum/model/Stats.java
@@ -1,15 +1,15 @@
 package ru.practicum.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import jakarta.persistence.*;
-import lombok.NoArgsConstructor;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Builder
 @AllArgsConstructor
@@ -28,4 +28,22 @@ public class Stats {
     private String ip;
     @Column(name = "time_stamp", nullable = false)
     private LocalDateTime timestamp;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Stats stats = (Stats) o;
+        return getId() != null && Objects.equals(getId(), stats.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
+                : getClass().hashCode();
+    }
 }
